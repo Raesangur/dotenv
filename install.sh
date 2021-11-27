9f35e90176d1f2e647056766c060b6076e407872
Raesangur <pascalemmanuel.lachance@gmail.com>
2021-11-27 10:46:46 -0500

    neofetch config files; changed distro to UwUntu

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 echo "Creating symbolic links..."
 ln -s ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -s ~/dotfiles/neofetch/config.conf ~/.config/neofetch/config.conf
 
 
 echo "Enter \$source ~/.zshrc" to configure zsh
