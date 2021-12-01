7a5e92bded2dd7c4eb9a6b5ac7d48b737a819cc6
Raesangur <pascalemmanuel.lachance@gmail.com>
2021-11-30 22:41:09 -0500

    updated git_gud script to take commit message as argument (this is also a test)

--- git/git_gud.sh
+++ git/git_gud.sh
@@ -1,7 +1,12 @@
 git add *
 
-echo 'Enter the commit message:'
-read commitMessage
+if [ $# -eq 0 ] ; then
+    echo 'Enter the commit message:'
+    read commitMessage
+else
+    commitMessage=$1
+fi
+
 
 git commit -m "$commitMessage"
 
